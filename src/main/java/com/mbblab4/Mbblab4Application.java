e5888d0b492c446a693db478fc01e83e3f0e2145
Ubuntu <prathish@RG1.dlfhmzcuz1xunf332gqam3f3bg.tx.internal.cloudapp.net>
2025-08-14 05:08:47 +0000

    testing auto trigger jenkins

--- src/main/java/com/mbblab4/Mbblab4Application.java
+++ src/main/java/com/mbblab4/Mbblab4Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Mbblab4Application {
 
 	public static void main(String[] args) {
+		System.out.println("My application");
 		SpringApplication.run(Mbblab4Application.class, args);
 	}
 
